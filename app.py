c155a1d2d5b5acdfeb4008875a3dbbfa174f9f7a
Jylie <escanajy@gmail.com>
2025-05-18 01:58:31 +0800

    feat: :sparkles: /api/chat integrated

--- app.py
+++ app.py
@@ -20,15 +20,25 @@ def chat():
     elif user_input == "other matters":
         reply = "Di ko pa masasagot yan eh. Balik ka nalang"
     elif user_input == "breakup" or user_input == "break up":
-        reply = "nako be, "
-    elif user_input == "bye" or user_input == "paalam":
-        reply = "Wag kana babalik"
-    elif user_input == "bakit":
+        reply = "nako be, This isn't the end of your story, just the close of a chapter. Embrace the pain, and then rise stronger than before. "
+    elif user_input == "cheating" or user_input == "cheater":
+        reply = "ikalma mo kase their actions say everything about them, and nothing about your worth. You deserve honesty and loyalty. Heal and find someone who truly values you. "
+    elif user_input == "crush" or user_input == "happy crush":
+        reply = "nge crush palang pala e. gawin mo kausapin mo tas jowain mo na kagad. gagraduate ka na wala ka paring jowa. KUMILOS KANA "
+    elif user_input == "walang kwenta" or user_input == "wala kang natulong te":
+        reply = "Be wala akong pake, di mo naman ako binabayaran. Try mo premium version ko para mas maganda sagot. 500 lang subscription hahanapan pa kita ng jowa"
+    elif user_input == "salamat" or user_input == "thanks":
+        reply = "gggg"
+    elif user_input == "nge" or user_input == "amp":
+        reply = "Sa barangay ka magreklamo."
+    elif user_input == "bakit" or user_input == "bat":
         reply = "Aba malay ko."
-    elif user_input == "bakit":
-        reply = "Aba malay ko."
+    elif user_input == "bat di ako crush ng crush ko" or user_input == "bat di niya ko gusto":
+        reply = "Aba malay ko. Siguro siya yung problema be, di ikaw. Wag mo masyadong isipin. aral ka muna"
+    elif user_input == "bye" or user_input == "ge":
+        reply = "osiya lumayas kana"
     else:
-        reply = "Sa tingin mo ba alam ko yan?"
+        reply = "Sa tingin mo ba alam ko yan? At sa tingin mo ba may pake ako?"
 
     return jsonify({"reply": reply})
 
