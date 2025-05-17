175dba19413d90345422bb708a1c1b51c9743822
Jylie <escanajy@gmail.com>
2025-05-18 01:43:05 +0800

    feat: :sparkles: /api/chat integrated

--- app.py
+++ app.py
@@ -13,10 +13,22 @@ def chat():
     data = request.get_json()
     user_input = data.get('message', '').lower()
 
-    if user_input == "hi":
-        reply = "hello"
+    if user_input == "hi" or user_input == "hello":
+        reply = "Anong maitutulong ko? type kung relationship or other matters"
+    elif user_input == "relationship":
+        reply = "Wala akong pake. Joke! Anong meron? Type kung breakup,cheating,crush"
+    elif user_input == "other matters":
+        reply = "Di ko pa masasagot yan eh. Balik ka nalang"
+    elif user_input == "breakup" or user_input == "break up":
+        reply = "nako be, "
+    elif user_input == "bye" or user_input == "paalam":
+        reply = "Wag kana babalik"
+    elif user_input == "bakit":
+        reply = "Aba malay ko."
+    elif user_input == "bakit":
+        reply = "Aba malay ko."
     else:
-        reply = "I don’t understand."
+        reply = "Sa tingin mo ba alam ko yan?"
 
     return jsonify({"reply": reply})
 
