aae4c677355a39feb64f74e51127f429106681da
Jylie <escanajy@gmail.com>
2025-05-18 01:06:12 +0800

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
-def hello():
+def home():
     return "Mama mo blue"
 
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_input = data.get('message', '').lower()
+
+    if user_input == "hi":
+        reply = "hello"
+    else:
+        reply = "I don’t understand."
+
+    return jsonify({"reply": reply})
+
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True)
